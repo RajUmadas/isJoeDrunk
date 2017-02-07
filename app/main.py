bdc33e31190a21f582909e19a626c71d001f7e55
Rajendra Umadas <raj@canary.is>
2017-02-07 17:03:18 -0500

    trying out mongo

--- app/main.py
+++ app/main.py
@@ -2,21 +2,41 @@
 
 from time import sleep
 from flask import Flask, render_template, redirect
+import os
+from pymongo import MongoClient
 
-from models.answers import Answers
-from models.detained import Detained
 APP = Flask(__name__)
+client = MongoClient(os.environ['DB_PORT_27017_TCP_ADDR'], 27017)
+db = client.db
 
 @APP.route('/')
 def show_answer():
     """Main entry point for the answer"""
-    ans = None
+    if db.answers.find().count()==0:
+        ans = "asdfasfasdf"
+    else:
+        ans = db.answers.find().sort([("_id", -1)]).limit(1)[0]['answer']
+
     return render_template('page.html', answer=ans)
 
 @APP.route('/set')
 def easyset():
     """Buttons for when joe is drunk"""
     return render_template('set.html')
+
+@APP.route('/set/no')
+def set_no():
+    """Setting No"""
+    db.answers.insert_one({'answer': 'No'})
+    sleep(0.5)
+    return redirect('/')
+
+@APP.route('/set/yes')
+def set_yes():
+    """Setting Yes"""
+    db.answers.insert_one({'answer': 'Yes'})
+    sleep(0.5)
+    return redirect('/')
 
 
 if __name__ == "__main__":

1549077e57efd5add6e51a7ff57ad4e1266640e9
Raj Umadas <raj.umadas@gmail.com>
2020-06-13 11:16:44 -0400

    Initial local postgres plumbing

---
+++ service/dbmigrate/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
+)
+
+func main() {
+	chanelEnv := os.Getenv("DEPLOY_CHANNEL")
+	if chanelEnv == "local-devel" {
+		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+			os.Exit(1)
+		}
+		defer conn.Close(context.Background())
+
+		var greeting string
+		err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(greeting)
+		return
+	}
+
+	fmt.Println("Dont know the env yo")
+
+}
